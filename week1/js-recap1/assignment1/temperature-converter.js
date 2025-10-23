3df068fa84a8cd89b3208718efcebee69411b6c9
zakke-username <zakkezweyg@gmail.com>
2025-10-23 22:46:18 +0300

    add js recap first assignment

---
+++ week1/js-recap1/assignment1/temperature-converter.js
@@ -0,0 +1,13 @@
+function celsiusToFahrenheit(degrees) {
+  return (degrees * (9 / 5)) + 32;
+}
+
+function celsiusToKelvin(degrees) {
+  return degrees + 273.15;
+}
+
+let celsius = parseFloat(prompt('Enter degrees in celsius for conversion:'));
+
+document.querySelector('#input').innerText = `Input = ${celsius}`;
+document.querySelector('#fahrenheit').innerText = `Fahrenheit degrees = ${celsiusToFahrenheit(celsius)}`;
+document.querySelector('#kelvin').innerText = `Kelvin degrees = ${celsiusToKelvin(celsius)}`;
