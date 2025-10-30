6f2fd660a1edd0a50b85faf30ab933f1b2798caf
zakke-username <zakkezweyg@gmail.com>
2025-10-30 22:01:35 +0200

    error handling and fixes to restaurant api assignment, week 2

--- week2/ajax/assignment5/restaurant-api.js
+++ week2/ajax/assignment5/restaurant-api.js
@@ -1,3 +1,4 @@
+// Fetch
 async function request(endpoint, options) {
   const API_URL = 'https://media2.edu.metropolia.fi/restaurant';
   let response = await fetch(`${API_URL}${endpoint}`, options);
@@ -5,6 +6,7 @@ async function request(endpoint, options) {
   return response.json();
 }
 
+// Restaurants API call
 async function getRestaurants() {
   try {
     let options = {
@@ -16,9 +18,11 @@ async function getRestaurants() {
     return restaurants;
   } catch (error) {
     console.error(error);
+    return [];
   }
 }
 
+// Menu API call
 async function getMenu(id) {
   try {
     let options = {
@@ -30,11 +34,36 @@ async function getMenu(id) {
     return menu;
   } catch (error) {
     console.error(error);
+    return [];
   }
 }
 
+// Display list of restaurants
 function displayRestaurants(restaurants) {
+  if (!restaurants || restaurants.length === 0) {
+    let errorMessage = document.createElement('p');
+    errorMessage.innerText = 'Error: could not find restaurants.';
+    document.querySelector('main').appendChild(errorMessage);
+    return;
+  }
+
   const table = document.querySelector('table');
+  table.innerHTML = '';
+
+  // Headers
+  let headers = document.createElement('tr');
+
+  let nameHeader = document.createElement('th');
+  nameHeader.innerText = 'Name';
+  headers.appendChild(nameHeader);
+
+  let addressHeader = document.createElement('th');
+  addressHeader.innerText = 'Address';
+  headers.appendChild(addressHeader);
+
+  table.appendChild(headers);
+
+  // Restaurant name & address
   for (let r of restaurants) {
     let tr = document.createElement('tr');
 
@@ -47,38 +76,24 @@ function displayRestaurants(restaurants) {
     tr.appendChild(address);
 
     tr.addEventListener('click', () => {
-      for (let row of table.children) {
+      for (let row of table.querySelectorAll('tr')) {
         row.classList.remove('highlight');
       }
       tr.classList.add('highlight');
-      displayInfo(r);
+      displayDialog(r);
     });
 
     table.appendChild(tr);
   }
 }
 
-async function displayInfo(r) {
+// Display restaurant modal (info & menu)
+async function displayDialog(r) {
   let dialog = document.querySelector('dialog');
   dialog.innerHTML = '';
 
-  let name = document.createElement('h3');
-  name.innerText = r.name;
-  dialog.appendChild(name);
-
-  let address = document.createElement('p');
-  address.innerText = `Address: ${r.address}, ${r.postalCode} ${r.city}`;
-  dialog.appendChild(address);
-
-  let phoneNumber = document.createElement('p');
-  phoneNumber.innerText = 'Phone: ' + r.phone;
-  dialog.appendChild(phoneNumber);
-
-  let company = document.createElement('p');
-  company.innerText = 'Company: ' + r.company;
-  dialog.appendChild(company);
-
-  dialog.appendChild(await displayMenu(r._id));
+  dialog.appendChild(createInfoBox(r));
+  dialog.appendChild(await createMenuTable(r._id));
 
   let closeButton = document.createElement('button');
   closeButton.innerText = 'Close';
@@ -88,12 +103,59 @@ async function displayInfo(r) {
   dialog.showModal();
 }
 
-async function displayMenu(id) {
+// General restaurant info div
+function createInfoBox(r) {
+  let infoBox = document.createElement('div');
+
+  let name = document.createElement('h3');
+  name.innerText = r.name;
+  infoBox.appendChild(name);
+
+  let address = document.createElement('p');
+  address.innerText = `Address: ${r.address}, ${r.postalCode} ${r.city}`;
+  infoBox.appendChild(address);
+
+  let phoneNumber = document.createElement('p');
+  phoneNumber.innerText = 'Phone: ' + r.phone;
+  infoBox.appendChild(phoneNumber);
+
+  let company = document.createElement('p');
+  company.innerText = 'Company: ' + r.company;
+  infoBox.appendChild(company);
+
+  return infoBox;
+}
+
+// Menu table
+async function createMenuTable(id) {
   let menu = await getMenu(id);
 
-  console.log(menu.courses);
+  if (!menu.courses || menu.courses.length === 0) {
+    let errorMessage = document.createElement('p');
+    errorMessage.innerText = 'Could not find menu.';
+    return errorMessage;
+  };
 
   let table = document.createElement('table');
+
+  // Headers
+  let headers = document.createElement('tr');
+
+  let nameHeader = document.createElement('th');
+  nameHeader.innerText = 'Course';
+  headers.appendChild(nameHeader);
+
+  let dietHeader = document.createElement('th');
+  dietHeader.innerText = 'Allergens';
+  headers.appendChild(dietHeader);
+
+  let priceHeader = document.createElement('th');
+  priceHeader.innerText = 'Price';
+  headers.appendChild(priceHeader);
+
+  table.appendChild(headers);
+
+  // Courses (name, allergens, price)
   for (let course of menu.courses) {
     let row = document.createElement('tr');
 
@@ -102,8 +164,12 @@ async function displayMenu(id) {
     row.appendChild(name);
 
     let diets = document.createElement('td');
-    diets.innerText = course.diets;
+    diets.innerText = course.diets ? course.diets : '';
     row.appendChild(diets);
+
+    let price = document.createElement('td');
+    price.innerText = course.price ? course.price : '';
+    row.appendChild(price);
 
     table.appendChild(row)
   }
@@ -111,7 +177,9 @@ async function displayMenu(id) {
 }
 
 async function main() {
+  // Get & sort restaurants
   let restaurants = await getRestaurants();
+  restaurants.sort((a, b) => a.name.localeCompare(b.name));
   displayRestaurants(restaurants);
 }
 
